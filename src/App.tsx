d8a57d0076b2d1a3499319a4b8159edbf008df3e
artyomka <artyomka1350@gmail.com>
2026-09-26 23:26:07 +0300

    урок 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,122 +1,59 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const pageTitle = "Читательский дневник";
+  const pageSubtitle = "Мои прочитанные и планируемые книги";
+  const totalBook = 3;
+  const readingBook = 1;
+  const completedBook = 1;
+
+  const plannedBook = totalBook - readingBook - completedBook;
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
+    <main className="page">
+      <header className="page__header">
+        <h1 className="page__title">{pageTitle}</h1>
+        <p className="page__descr">{pageSubtitle}</p>
+      </header>
+      <section className="stats">
+        <h2 className="stats__title">Статистика</h2>
+        <div className="stats__list">
+          <div className="stats__item">
+            <p>Всего книг: {totalBook}</p>
+          </div>
+          <div className="stats__item">
+            <p>Читаю сейчас: {readingBook}</p>
+          </div>
+          <div className="stats__item">
+            <p>Планирую: {plannedBook}</p>
+          </div>
+          <div className="stats__item">
+            <p>Закончено: {completedBook}</p>
+          </div>
         </div>
       </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+      <section className="books">
+        <h2 className="books__title">Мои книги</h2>
+        <ul className="books__list">
+          <li className="books__item">
+            <h3 className="books__name">1984</h3>
+            <p className="books__author">Джорд Оруэлл</p>
+            <p className="books__status">Читаю</p>
+          </li>
+          <li className="books__item">
+            <h3 className="books__name">Эхо стеклянного города</h3>
+            <p className="books__author">Максим Корсаков</p>
+            <p className="books__status">Читаю</p>
+          </li>
+          <li className="books__item">
+            <h3 className="books__name">Полночь в лавке забытых вещей</h3>
+            <p className="books__author">Элена Моран</p>
+            <p className="books__status">Читаю</p>
+          </li>
+        </ul>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
